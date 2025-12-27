b2c8c8cb72c72fe2cfe8b4915f4363676077e81b
OyatullohG24 <khudoyberganovoyatulloh405@gmail.com>
2025-12-27 19:41:14 +0500

    feat: replace LayoutGrid icon with Shirt icon for the Clothes navigation item

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -14,7 +14,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Shirt } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -26,7 +26,7 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Clothes',
         href: "/clothes",
-        icon: LayoutGrid,
+        icon: Shirt,
     },
 ];
 
